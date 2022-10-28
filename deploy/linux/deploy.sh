28c85ed6e1d8b8c3abba886ee7279ca0ca625da1
Valerii Herman <palanickchuk@meta.ua>
2022-10-28 11:43:08 +0300

    change folders from omnidb* to pgmanage*

--- deploy/linux/deploy.sh
+++ deploy/linux/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -e VERSION="1.0.0" -v $PWD:/tmp --rm omnidb/tarbuild
+docker run -e VERSION="1.0.0" -v $PWD:/tmp --rm pgmanage/tarbuild
 # docker run -e VERSION="3.0.6" -v $PWD:/tmp --rm omnidb/pkgbuild
 
 sudo chown $USER:$USER *.tar.gz
--- deploy/linux/tarbuild/entrypoint.sh
+++ deploy/linux/tarbuild/entrypoint.sh
@@ -14,31 +14,31 @@ pip3 install -r requirements.txt
 
 # Building server
 cd OmniDB/
-rm -f omnidb.db omnidb.log
-touch omnidb.db
+rm -f pgmanage.db pgmanage.log
+touch pgmanage.db
 pyinstaller OmniDB-lin.spec
 mv dist/omnidb-server $HOME
 rm -rf build dist
 cd $HOME
-mkdir omnidb-server_$VERSION
-cp omnidb-server omnidb-server_$VERSION/
-tar -czvf omnidb-server_$VERSION.tar.gz omnidb-server_$VERSION/
-mv omnidb-server_$VERSION.tar.gz /tmp/
+mkdir pgmanage-server_$VERSION
+cp omnidb-server pgmanage-server_$VERSION/
+tar -czvf pgmanage-server_$VERSION.tar.gz pgmanage-server_$VERSION/
+mv pgmanage-server_$VERSION.tar.gz /tmp/
 
 # Building app
 curl -LO https://dl.nwjs.io/v0.69.1/nwjs-v0.69.1-linux-x64.tar.gz
 tar -xzvf nwjs-v0.69.1-linux-x64.tar.gz
-mv nwjs-v0.69.1-linux-x64 omnidb-app_$VERSION
-cd omnidb-app_$VERSION
+mv nwjs-v0.69.1-linux-x64 pgmanage-app_$VERSION
+cd pgmanage-app_$VERSION
 rm ./lib/libEGL.so
 rm ./lib/libGLESv2.so
 rm ./lib/libvulkan.so.1
 rm ./lib/libvk_swiftshader.so
 rm ./lib/*.json
-mkdir omnidb-server
-cp $HOME/omnidb-server ./omnidb-server/
+mkdir pgmanage-server
+cp $HOME/omnidb-server ./pgmanage-server/
 cp $HOME/OmniDB/deploy/app/* .
 mv nw omnidb-app
 cd $HOME
-tar -czvf omnidb-app_$VERSION.tar.gz omnidb-app_$VERSION/
-mv omnidb-app_$VERSION.tar.gz /tmp/
+tar -czvf pgmanage-app_$VERSION.tar.gz pgmanage-app_$VERSION/
+mv pgmanage-app_$VERSION.tar.gz /tmp/
