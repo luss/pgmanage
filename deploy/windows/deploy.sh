037c4f2f645e66289282d8b494ee1459eb9688e8
Eugene Dubinin <eugend@commandprompt.com>
2022-11-28 16:38:04 +0200

    windows build scripts, WIP new artwork windows icon files

--- deploy/windows/deploy.sh
+++ deploy/windows/deploy.sh
@@ -1,6 +1,6 @@
 #this script is intended to be run under cygwin + win10 environment
 set -e -e
-pushd .
+
 APP_VERSION="$1"
 
 # if version is not provided, we use last tag from repository
@@ -15,41 +15,43 @@ fi
 
 APP_LONG_VERSION=PgManage.$APP_VERSION
 
-echo "making release dir: release_$APP_VERSION"
-rm -rf release_$APP_VERSION tmp
-mkdir release_$APP_VERSION tmp
+# echo "making release dir: release_$APP_VERSION"
+# rm -rf release_$APP_VERSION tmp
+# mkdir release_$APP_VERSION tmp
 
-# Prepare temporary directory
-echo "copying app files into tmp"
-cp -R ../../OmniDB/* tmp
-cd tmp/
-rm -rf pgmanage.db pgmanage.log
-touch pgmanage.db
+# # Prepare temporary directory
+# echo "copying app files into tmp"
+# cp -R ../../OmniDB/* tmp
+# cd tmp/
+# rm -rf pgmanage.db pgmanage.log
+# touch pgmanage.db
 
-# Install all required libraries
-pip3 install -r ../../../requirements.txt
-pip3 install pyinstaller
+# # Install all required libraries
+# pip3 install -r ../../../requirements.txt
+# pip3 install pyinstaller
 
-# set up versions in custom_settins.py
-echo "setting app version in sources to $APP_VERSION"
-sed -i "s/Dev/PgManage $APP_VERSION/" OmniDB/custom_settings.py
-sed -i "s/dev/$APP_VERSION/" OmniDB/custom_settings.py
+# # set up versions in custom_settins.py
+# echo "setting app version in sources to $APP_VERSION"
+# sed -i "s/Dev/PgManage $APP_VERSION/" OmniDB/custom_settings.py
+# sed -i "s/dev/$APP_VERSION/" OmniDB/custom_settings.py
 
-echo "running pyinstaller"
-/cygdrive/c/Users/$USER/AppData/Roaming/Python/Python38/Scripts/pyinstaller.exe ./omnidb-win.spec
-mv dist/pgmanage-server ../release_$APP_VERSION/
-# exit 0
-cd ..
-curl -C - https://dl.nwjs.io/v0.69.1/nwjs-v0.69.1-win-x64.zip -o nwjs.zip
-unzip -o nwjs.zip -d tmp/
+# echo "running pyinstaller"
+# /cygdrive/c/Users/$USER/AppData/Roaming/Python/Python38/Scripts/pyinstaller.exe ./omnidb-win.spec
+# mv dist/pgmanage-server ../release_$APP_VERSION/
 
-mv tmp/nwjs-v0.69.1-win-x64/* release_$APP_VERSION/
-mv release_$APP_VERSION/nw.exe release_$APP_VERSION/pgmanage-app.exe
-cp ../app/index.html release_$APP_VERSION
-cp ../app/package.json release_$APP_VERSION
-cp ../app/omnidb_icon.png release_$APP_VERSION
+# cd ..
+# curl -C - https://dl.nwjs.io/v0.69.1/nwjs-v0.69.1-win-x64.zip -o nwjs.zip
+# unzip -o nwjs.zip -d tmp/
+
+# mv tmp/nwjs-v0.69.1-win-x64/* release_$APP_VERSION/
+# mv release_$APP_VERSION/nw.exe release_$APP_VERSION/pgmanage-app.exe
+# cp ../app/index.html release_$APP_VERSION
+# cp ../app/package.json release_$APP_VERSION
+# cp ../app/omnidb_icon.png release_$APP_VERSION
 
 sed -i "s/version_placeholder/v$APP_VERSION/" release_$APP_VERSION/index.html
-# #../../../../../../Arquivos\ de\ Programas/Resource\ Hacker/ResourceHacker.exe -open release/OmniDB/omnidb-app.exe -save release/OmniDB/omnidb-app.exe -action addoverwrite -res ../win-icon.ico -mask ICONGROUP,IDR_MAINFRAME,
-# #../../../../../../Arquivos\ de\ Programas/Resource\ Hacker/ResourceHacker.exe -open release/OmniDB/omnidb-server/omnidb-server.exe -save release/OmniDB/omnidb-server/omnidb-server.exe -action addoverwrite -res ../win-icon.ico -mask ICONGROUP,IDR_MAINFRAME,
-rm -rf tmp
+# install resource hacker with default path, then uncomment these lines to replace the default pyinstaller and nwjs exe icons
+
+/cygdrive/c/Program\ Files\ \(x86\)/Resource\ Hacker/ResourceHacker.exe -open release_$APP_VERSION/pgmanage-app.exe -save release_$APP_VERSION/pgmanage-app.exe -action addoverwrite -res ./win-icon.ico -mask ICONGROUP,IDR_MAINFRAME,
+/cygdrive/c/Program\ Files\ \(x86\)/Resource\ Hacker/ResourceHacker.exe -open release_$APP_VERSION/pgmanage-server/pgmanage-server.exe -save release_$APP_VERSION/pgmanage-server/pgmanage-server.exe -action addoverwrite -res ./win-icon.ico -mask ICONGROUP,IDR_MAINFRAME,
+# rm -rf tmp
