8dc4b28eb4fff1ecc5b6646de7d3ceb9771cf738
Valerii Herman <palanickchuck@meta.ua>
2023-06-06 16:15:30 +0300

    remove unused tree.py file

--- pgmanage/app/views/__init__.py
+++ pgmanage/app/views/__init__.py
@@ -1,4 +1,4 @@
 from . import (backup, bgjob, commands_history, configuration, connections,
                file_manager, login, monitor_dashboard, plugins, polling,
-               restore, tree, tree_mariadb, tree_mysql, tree_oracle,
+               restore, tree_mariadb, tree_mysql, tree_oracle,
                tree_postgresql, tree_snippets, tree_sqlite, users, workspace)
