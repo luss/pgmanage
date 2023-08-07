c396d7bfda506ba407226225497d0a67e6a8fc71
Valerii Herman <palanickchuck@meta.ua>
2023-08-07 15:00:19 +0300

    remove vue3-sfc-loader, fix vue-toast-notification

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/main.js
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/main.js
@@ -1,4 +1,6 @@
 import 'vite/modulepreload-polyfill';
 import './workspace'
 import './components/postgresql_modals'
+import '@onekiloparsec/vue-power-tree/dist/style.css'
+import 'vue-toast-notification/dist/theme-sugar.css';
 console.log('Hello from Vite')
