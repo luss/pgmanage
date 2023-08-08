de3148e1fd3565d38c52afd5770cc324cf13cad0
Valerii Herman <palanickchuck@meta.ua>
2023-08-08 10:19:35 +0300

    remove xterm.css file

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/main.js
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/main.js
@@ -3,4 +3,5 @@ import './workspace'
 import './components/postgresql_modals'
 import '@onekiloparsec/vue-power-tree/dist/style.css'
 import 'vue-toast-notification/dist/theme-sugar.css';
+import 'xterm/css/xterm.css'
 console.log('Hello from Vite')
