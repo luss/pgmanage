6a8db206ae368d3fafc8c5568e134219ee9e5138
Eugene Dubinin <eugend@commandprompt.com>
2023-09-20 13:52:19 +0300

    improve ERD container sizing

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/components/ERDTab.vue
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/components/ERDTab.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 100%; height: 100vh; visibility: hidden" ref="cyContainer"></div>
+  <div style="width: 100%; height: calc(100vh - 70px); visibility: hidden" ref="cyContainer"></div>
 </template>
 
 <script>
