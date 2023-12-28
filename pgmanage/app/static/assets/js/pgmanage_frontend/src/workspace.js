62774651cf369f90c312d6f2ed3160faea7d4db7
Eugene Dubinin <eugend@commandprompt.com>
2023-12-28 14:50:18 +0200

    improve snippets editor resizing for different UI font sizes

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/workspace.js
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/workspace.js
@@ -444,7 +444,7 @@ var resizeSnippetPanel = async function(p_left_pos_x = false) {
         }
         v_snippet_tag.divPanel.style.height = v_target_tag_div_result_top + 'px';
         v_snippet_tag.divTree.style.height = v_target_tag_div_result_top + 'px';
-        v_inner_snippet_tag.editorDiv.style.height = v_target_tag_div_result_top - (7)*v_font_size + 'px';
+        v_inner_snippet_tag.editorDiv.style.height = v_target_tag_div_result_top - $(v_snippet_tag.divRight).find('.row').height() * 2 + 'px'
         v_inner_snippet_tag.editor.resize();
       }
     });
@@ -707,7 +707,6 @@ $(window).resize(function() {
 /// Refresh divs sizes and components of the currently selected tab
 /// </summary>
 function refreshHeights(p_all) {
-
   setTimeout(function(){
     //Adjusting tree height
     // if (p_all) {
