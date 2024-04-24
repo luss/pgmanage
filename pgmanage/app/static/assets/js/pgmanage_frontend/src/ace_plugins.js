807362c6451631340f08f1713ed283ae5b7f80dd
plucik <deliverycrunchtime@gmail.com>
2024-04-24 13:39:46 +0300

    escape regexp special characters from selected text before pass it a regexp pattern

--- pgmanage/app/static/assets/js/pgmanage_frontend/src/ace_plugins.js
+++ pgmanage/app/static/assets/js/pgmanage_frontend/src/ace_plugins.js
@@ -102,22 +102,23 @@ function setupAceDragDrop(editor, isSnippetTab = false) {
 }
 
 function setupAceSelectionHighlight(editor) {
-    editor.selection.on("changeSelection", () => {
-      // Get the selected text
-      let selectedText = editor.getSelectedText();
-  
-      // Clear existing highlights
-      editor.getSession().clearAnnotations();
-  
-      if (selectedText.length > 0) {
-        // Find other occurrences of the selected text
-        let pattern = new RegExp(selectedText, "g");
-  
-        editor.getSession().highlight(pattern);
-        editor.getSession().$searchHighlight.clazz = "ace_selection";
-      }
-    });
-  }
-  
+  editor.selection.on("changeSelection", () => {
+    // Get the selected text
+    let selectedText = editor.getSelectedText();
+
+    // Clear existing highlights
+    editor.getSession().clearAnnotations();
+
+    if (selectedText.length > 0) {
+      let escapedText = selectedText.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+      // Find other occurrences of the selected text
+      let pattern = new RegExp(escapedText, "g");
+
+      editor.getSession().highlight(pattern);
+      editor.getSession().$searchHighlight.clazz = "ace_selection";
+    }
+  });
+}
 
 export { setupAceDragDrop, setupAceSelectionHighlight };
