8e155818a1e04a2d9069ef1fefc082ccf903f60f
plucik <deliverycrunchtime@gmail.com>
2024-10-11 13:08:57 +0300

    set key_manager keys in debug mode from PGMANAGE_USER_KEYS enviroment variable

--- pgmanage/app/utils/key_manager.py
+++ pgmanage/app/utils/key_manager.py
@@ -1,3 +1,6 @@
+from pgmanage import settings
+
+
 class KeyManager:
 
     __users = dict()
@@ -5,20 +8,32 @@ class KeyManager:
     def get(self, current_user):
         user = self.__users.get(current_user.id, None)
         if user is not None:
-            return user.get('key', None)
+            return user.get("key", None)
         return None
-
 
     def set(self, current_user, _key):
         user = self.__users.get(current_user.id, None)
         if user is None:
-            self.__users[current_user.id] = dict(key = _key)
+            self.__users[current_user.id] = dict(key=_key)
         else:
-            user['key'] = _key
+            user["key"] = _key
 
     def remove(self, current_user):
         user = self.__users.get(current_user.id, None)
         if user is not None:
             del self.__users[current_user.id]
 
+
 key_manager = KeyManager()
+
+if settings.DEBUG:
+    import os
+    from django.contrib.auth import get_user_model
+
+    user_keys_env = os.getenv("PGMANAGE_USER_KEYS")
+    if user_keys_env:
+        for user_key_pair in user_keys_env.split("|"):
+            user_id, user_key = user_key_pair.split(":")
+            user = get_user_model().objects.filter(id=user_id).first()
+            if user:
+                key_manager.set(user, user_key)
