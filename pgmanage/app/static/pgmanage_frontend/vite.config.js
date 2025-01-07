078c4ed549c2f8820c1788b8321d439fcfe8e48f
plucik <deliverycrunchtime@gmail.com>
2025-01-07 13:38:14 +0200

    adds hash in resulting build file names, removes redundant assets from login.html and workspace.html

--- pgmanage/app/static/pgmanage_frontend/vite.config.js
+++ pgmanage/app/static/pgmanage_frontend/vite.config.js
@@ -43,9 +43,9 @@ export default defineConfig(({ command, mode }) => {
       rollupOptions: {
         input: ["./src/main.js", "./src/login.js"],
         output: {
-          entryFileNames: `assets/[name].js`,
-          chunkFileNames: `assets/[name].js`,
-          assetFileNames: `assets/[name].[ext]`,
+          entryFileNames: `assets/[name].[hash].js`,
+          chunkFileNames: `assets/[name].[hash].js`,
+          assetFileNames: `assets/[name].[hash].[ext]`,
         },
       },
       outDir: outDir,
