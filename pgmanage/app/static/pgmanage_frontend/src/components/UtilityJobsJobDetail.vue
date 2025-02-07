64e7f17f66d60a44080a677de74d42834feb980c
plucik <deliverycrunchtime@gmail.com>
2025-02-07 17:39:38 +0200

    fixes issue when job detail could show detail of another job

--- pgmanage/app/static/pgmanage_frontend/src/components/UtilityJobsJobDetail.vue
+++ pgmanage/app/static/pgmanage_frontend/src/components/UtilityJobsJobDetail.vue
@@ -88,6 +88,7 @@ export default {
       axios
         .get(`/bgprocess/${job_id}/${out}/${err}/`)
         .then((resp) => {
+          if (!Object.keys(this.selectedJob).length) return
           this.out = resp.data.data.out.pos;
           this.err = resp.data.data.err.pos;
           utilityJobStore.setDuration(resp.data.data.duration);
@@ -124,6 +125,8 @@ export default {
       this.detailJobWorkerId = "";
       this.out = 0;
       this.err = 0;
+      clearInterval(this.detailJobWorkerId);
+      this.detailJobWorkerId = "";
     },
     scrollToBottom() {
       this.$nextTick(() => {
